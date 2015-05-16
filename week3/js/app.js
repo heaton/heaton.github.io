516331c3c48372747539623f67f148b7dc934dfd
Heaton Cai <heaton.cai@gmail.com>
2015-05-17 01:44:56 +0800

    support touch screen

--- week3/js/app.js
+++ week3/js/app.js
@@ -8,10 +8,39 @@ $(function() {
     $('.movies .active').nextAll().addClass('at-right');
   });
 
+  function slide(direction) {
+    var f = direction == 'left' ? 'prev' : 'next';
+    $('.chronology-nav ul li.active')[f]().click();
+  }
+
   $(document).keydown(function(e) {
     switch(e.keyCode) {
-      case 37: e.preventDefault();$('.chronology-nav ul li.active').prev().click(); break;
-      case 39: e.preventDefault();$('.chronology-nav ul li.active').next().click(); break;
+      case 37: e.preventDefault(); slide('left'); break;
+      case 39: e.preventDefault(); slide('right'); break;
     };
   });
+
+  var oriX;
+  var touch = function(e) {
+    var oe = e.originalEvent;
+    return oe.touches[0] || oe.changedTouches[0];
+  };
+  var endEvent = function(e) {
+    $('.movies article').css('transition', '').css('transform', '');
+    var moveDistance = touch(e).screenX - oriX;
+    if(moveDistance > 30) {
+      slide('left');
+    } else if(moveDistance < -30) {
+      slide('right');
+    }
+  };
+  $('.movies article').on('touchstart', function(e) {
+    oriX = touch(e).screenX;
+    $('.movies article').css('transition', 'none');
+  }).on('touchmove', function(e) {
+    var moveDistance = touch(e).screenX - oriX;
+    $(this).css('transform', 'translate(' + moveDistance + 'px)');
+    $(this).prev().css('transform', 'translate(' + (-$(window).width() + moveDistance) + 'px)');
+    $(this).next().css('transform', 'translate(' + ($(window).width() + moveDistance) + 'px)');
+  }).on('touchend', endEvent).on('touchcancel', endEvent);
 });
